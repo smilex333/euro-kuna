fe8c8d749d1e28e7ae20cefcfa4c790c0d8cfd5a
s3m <142555127+smilex333@users.noreply.github.com>
2023-09-22 13:13:18 +0200

    Add files via upload

---
+++ euro.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+7,53450 kn = 1 euro
+
+1 kn = 0,132722808 eura
+
+1 kn = 100 lipa
+
+1 euro = 100 centi
+
+
+
+kuna=raw_input("UPISI KUNE: ")
+
+euro=1/7.53450
+
+
+print(euro)
---
+++ kuna.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+7.53450 kn = 1 euro
+
+1 kn = 0.132722808 eura
+
+1 kn = 100 lipa
+
+1 euro = 100 centi
+"""
+
+euro=raw_input("UPISI EURE: ")
+
+eurof=float(euro)
+
+eurof=eurof*7.53450
+
+kuna=eurof
+
+print(kuna)
+
+print("KUNA")
