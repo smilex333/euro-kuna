90bbab6e15da672f4050a5990104dbc6807f7b6a
s3m <142555127+smilex333@users.noreply.github.com>
2023-10-04 22:51:11 +0200

    Add files via upload

--- euro.py
+++ euro.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 
-7,53450 kn = 1 euro
+"""
+7.53450 kn = 1 euro
 
-1 kn = 0,132722808 eura
+1 kn = 0.132722808 eura
 
 1 kn = 100 lipa
 
 1 euro = 100 centi
-
-
+"""
 
 kuna=raw_input("UPISI KUNE: ")
 
+kunaf=float(kuna)
+
 euro=1/7.53450
 
+kunaf=kunaf*0.132722808
+
+euro=kunaf
 
 print(euro)
+
+print("EURA")
